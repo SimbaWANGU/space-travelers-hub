425dfa0595db87183999307960c2c4db5939c00e
christian <christianbiringanine22@gmail.com>
2022-06-13 16:17:05 +0200

    Add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Rockets</li>
+        <li>Mission</li>
+        <li>Dragons</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
