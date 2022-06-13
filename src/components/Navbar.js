850e10381b89e521492f66fa7334d15cedee76ea
SimbaWANGU <simbawangulu@gmail.com>
2022-06-13 18:22:35 +0300

    Use navlink for navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import { NavLink, Routes, Route } from 'react-router-dom';
+import Profile from '../pages/Profile';
+import Dragons from '../pages/Dragons';
+import Mission from '../pages/Mission';
+import Rockets from '../pages/Rockets';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>Rockets</li>
-        <li>Mission</li>
-        <li>Dragons</li>
-      </ul>
-    </nav>
+    <>
+      <div className="nav">
+        <NavLink
+          to="dragons"
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
+          Dragons
+        </NavLink>
+        <NavLink
+          to="missions"
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
+          Missions
+        </NavLink>
+        <NavLink
+          to="rockets"
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
+          Rockets
+        </NavLink>
+        <NavLink
+          to="profile"
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
+          Profile
+        </NavLink>
+      </div>
+      <Routes>
+        <Route path="dragons" element={<Dragons />} />
+        <Route path="missions" element={<Mission />} />
+        <Route path="rockets" element={<Rockets />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 
