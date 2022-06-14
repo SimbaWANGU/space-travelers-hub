c093c439751b5699238f1d87ddd6d5251a85482f
SimbaWANGU <simbawangulu@gmail.com>
2022-06-14 12:14:14 +0300

    Fetch data from API

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import fetchRockets from '../api/rocketApiCall';
 import RocketContainer from '../components/RocketContainer';
+import { getRockets } from '../redux/rockets/rockets';
 
 function Rockets() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function data() {
+      const rockets = await fetchRockets();
+      dispatch(getRockets(rockets));
+    }
+    data();
+  }, []);
+
   return (
     <>
       <RocketContainer />
